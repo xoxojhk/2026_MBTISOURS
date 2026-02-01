e871d3993c46009e881d60c11c7ac2c89df34241
xoxojhk <9lovejhk@naver.com>
2026-02-01 15:54:51 +0900

    First Commit

---
+++ src/main/java/com/mbtisours/MbtisoursApplication.java
@@ -0,0 +1,13 @@
+package com.mbtisours;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MbtisoursApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MbtisoursApplication.class, args);
+	}
+
+}
